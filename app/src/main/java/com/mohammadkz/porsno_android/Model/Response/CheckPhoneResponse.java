0ef71e3555feda0904ac97acd69ee74d32d92012
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-07-16 13:07:52 +0430

    item added

--- app/src/main/java/com/mohammadkz/porsno_android/Model/Response/CheckPhoneResponse.java
+++ app/src/main/java/com/mohammadkz/porsno_android/Model/Response/CheckPhoneResponse.java
@@ -1,4 +1,21 @@
 package com.mohammadkz.porsno_android.Model.Response;
 
 public class CheckPhoneResponse {
+    private String status_code , message;
+
+    public String getStatus_code() {
+        return status_code;
+    }
+
+    public void setStatus_code(String status_code) {
+        this.status_code = status_code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- app/src/main/java/com/mohammadkz/porsno_android/Model/Response/LoginResponse.java
+++ app/src/main/java/com/mohammadkz/porsno_android/Model/Response/LoginResponse.java
@@ -1,4 +1,77 @@
 package com.mohammadkz.porsno_android.Model.Response;
 
 public class LoginResponse {
+    private String message, status_code, userName, phoneNumber, birthday, accountLevel, created, end , id;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus_code() {
+        return status_code;
+    }
+
+    public void setStatus_code(String status_code) {
+        this.status_code = status_code;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAccountLevel() {
+        return accountLevel;
+    }
+
+    public void setAccountLevel(String accountLevel) {
+        this.accountLevel = accountLevel;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
