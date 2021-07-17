b613a14085a1ba5375c30b43d5fb290b73dd7ae1
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-07-17 15:11:50 +0430

    main page acitviy created

---
+++ app/src/main/java/com/mohammadkz/porsno_android/Activity/MainPageActivity.java
@@ -0,0 +1,16 @@
+package com.mohammadkz.porsno_android.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mohammadkz.porsno_android.R;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
