2083ebe90a7e3968123ebd5ccb7163f994b1bdb2
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-08-03 12:12:20 +0430

    profile fragment created

---
+++ app/src/main/java/com/mohammadkz/porsno_android/Fragment/ProfileFragment.java
@@ -0,0 +1,31 @@
+package com.mohammadkz.porsno_android.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mohammadkz.porsno_android.R;
+
+public class ProfileFragment extends Fragment {
+
+    View view;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        return view;
+    }
+}
