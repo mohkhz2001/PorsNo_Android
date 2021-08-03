377c6c58a0b53ef33e0a3f665957aeaeca27c30d
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-08-03 13:16:30 +0430

    main fragment => all question and user question

---
+++ app/src/main/java/com/mohammadkz/porsno_android/Fragment/MainFragment.java
@@ -0,0 +1,30 @@
+package com.mohammadkz.porsno_android.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mohammadkz.porsno_android.R;
+
+
+public class MainFragment extends Fragment {
+
+    View view;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+}
