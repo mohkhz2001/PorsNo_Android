023ec12c2a8819f817225424213b61d3c8c29dbf
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-08-13 15:36:29 +0430

    I added the bottom nav in the main activity but shoudn't => created the new fragment to for the bottom nav in xml some text Appearance added for font and etc

--- app/src/main/java/com/mohammadkz/porsno_android/Activity/MainPageActivity.java
+++ app/src/main/java/com/mohammadkz/porsno_android/Activity/MainPageActivity.java
@@ -7,6 +7,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -17,9 +18,15 @@ import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+import com.google.gson.Gson;
+import com.mohammadkz.porsno_android.Fragment.HistoryFragment;
+import com.mohammadkz.porsno_android.Fragment.MainFragment;
 import com.mohammadkz.porsno_android.Fragment.MyQuestionFragment;
+import com.mohammadkz.porsno_android.Fragment.ProfileFragment;
+import com.mohammadkz.porsno_android.Fragment.ReBuyFragment;
 import com.mohammadkz.porsno_android.Model.User;
 import com.mohammadkz.porsno_android.R;
+import com.mohammadkz.porsno_android.StaticFun;
 
 import org.json.JSONObject;
 
@@ -28,7 +35,7 @@ public class MainPageActivity extends AppCompatActivity {
     DrawerLayout drawer_layout;
     MaterialToolbar topAppBar;
     NavigationView nav_view;
-    BottomNavigationView bottom_nav;
+
     TextView name, phoneNumber;
     View header;
     User user;
@@ -53,7 +60,6 @@ public class MainPageActivity extends AppCompatActivity {
         drawer_layout = findViewById(R.id.drawer_layout);
         topAppBar = findViewById(R.id.topAppBar);
         nav_view = findViewById(R.id.nav_view);
-        bottom_nav = findViewById(R.id.bottom_nav);
 
         header = nav_view.getHeaderView(0);
         name = header.findViewById(R.id.user_name);
@@ -72,20 +78,50 @@ public class MainPageActivity extends AppCompatActivity {
 
         });
 
-        bottom_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                 switch (item.getItemId()) {
-                    case R.id.myQuestion:
+                    case R.id.main:
+                        nav_view.setCheckedItem(R.id.main);
                         startFragment();
                         break;
+                    case R.id.newQuestion:
+                        nav_view.setCheckedItem(R.id.newQuestion);
+                        Intent intent = new Intent(getApplicationContext(), NewQuestionActivity.class);
+                        Gson gson = new Gson();
+                        String a = gson.toJson(user);
+                        intent.putExtra("userInfo", a);//user.getID()
+                        startActivity(intent);
+                        break;
+                    case R.id.account:
+                        nav_view.setCheckedItem(R.id.account);
+                        ProfileFragment profileFragment = new ProfileFragment(user);
+                        fragmentTransaction.replace(R.id.frameLayout, profileFragment).commit();
+                        break;
+                    case R.id.reBuy:
+                        nav_view.setCheckedItem(R.id.reBuy);
+                        ReBuyFragment reBuyFragment = new ReBuyFragment(user);
+                        fragmentTransaction.replace(R.id.frameLayout , reBuyFragment).commit();
+                        break;
+                    case R.id.history:
+                        nav_view.setCheckedItem(R.id.history);
+                        HistoryFragment historyFragment = new HistoryFragment();
+                        fragmentTransaction.replace(R.id.frameLayout , historyFragment).commit();
+                        break;
+                    case R.id.about:
 
-                    case R.id.allQuestion:
+                        break;
+                    case R.id.contact:
 
+                        break;
+                    case R.id.exit:
+                        finish();
                         break;
                 }
 
+                drawer_layout.closeDrawer(GravityCompat.START);
                 return true;
             }
         });
@@ -93,8 +129,8 @@ public class MainPageActivity extends AppCompatActivity {
 
     private void startFragment() {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        MyQuestionFragment myQuestionFragment = new MyQuestionFragment(user);
-        fragmentTransaction.replace(R.id.frameLayout, myQuestionFragment).commit();
+        MainFragment mainFragment = new MainFragment(user);
+        fragmentTransaction.replace(R.id.frameLayout, mainFragment).commit();
     }
 
     // get data from sign up page(intent)
@@ -112,11 +148,34 @@ public class MainPageActivity extends AppCompatActivity {
                 user.setCreatedTime(jsonObject.getString("createdTime"));
                 user.setEndTime(jsonObject.getString("endTime"));
 
-                // account level ....
+                if (jsonObject.getString("accountLevel").equals("Bronze")) {
+                    user.setAccountLevel(StaticFun.account.Bronze);
+                } else if (jsonObject.getString("accountLevel").equals("Steel")) {
+                    user.setAccountLevel(StaticFun.account.Steel);
+                } else if (jsonObject.getString("accountLevel").equals("Gold")) {
+                    user.setAccountLevel(StaticFun.account.Gold);
+                } else if (jsonObject.getString("accountLevel").equals("Diamond")) {
+                    user.setAccountLevel(StaticFun.account.Diamond);
+                }
+
+                setHeaderValue();
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
     }
 
+    private void setHeaderValue(){
+        name.setText(user.getName().toString());
+        phoneNumber.setText(user.getPn().toString());
+    }
+
+    public void updateUser(User user){
+        name.setText(user.getName().toString());
+        this.user = user;
+    }
+
+    public void setDrawerSelect(int id){
+        nav_view.setCheckedItem(id);
+    }
 }
