3fa43078f36c2c00c5249ba7d8be2fb8895669ad
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-08-14 01:00:14 +0430

    the price of the account

---
+++ app/src/main/java/com/mohammadkz/porsno_android/Model/PriceResponse.java
@@ -0,0 +1,21 @@
+package com.mohammadkz.porsno_android.Model;
+
+public class PriceResponse {
+    private String accountLevel, cost;
+
+    public String getAccountLevel() {
+        return accountLevel;
+    }
+
+    public void setAccountLevel(String accountLevel) {
+        this.accountLevel = accountLevel;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+}
