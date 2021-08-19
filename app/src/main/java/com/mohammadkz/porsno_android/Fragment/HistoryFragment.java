3e22128e7f9e641bbcd16921cca71e0cf5b16cf9
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-08-19 23:39:23 +0430

    fragment of history that contain but and done

---
+++ app/src/main/java/com/mohammadkz/porsno_android/Fragment/HistoryFragment.java
@@ -0,0 +1,119 @@
+package com.mohammadkz.porsno_android.Fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+import com.mohammadkz.porsno_android.Model.User;
+import com.mohammadkz.porsno_android.R;
+
+import java.util.ArrayList;
+
+public class HistoryFragment extends Fragment {
+
+    View view;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    MainAdapter adapter;
+    User user;
+    public HistoryFragment(User user) {
+        // Required empty public constructor
+        this.user = user;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        initViews();
+        controllerViews();
+        setAdapter();
+
+        return view;
+    }
+
+    private void initViews() {
+        tabLayout = view.findViewById(R.id.tab_layout);
+        viewPager = view.findViewById(R.id.viewPager);
+    }
+
+    private void controllerViews() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 1){
+                    tabLayout.setSelectedTabIndicator(R.drawable.bg_tab_select_l);
+                }else {
+                    tabLayout.setSelectedTabIndicator(R.drawable.bg_tab_select_r);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void setAdapter(){
+        adapter = new MainAdapter(getFragmentManager());
+
+        adapter.addFragment(new History_BuyFragment(user) , "خرید ها");
+        adapter.addFragment(new History_DoneFragment(user) , "انجام شده");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private class MainAdapter extends FragmentPagerAdapter {
+        ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+        ArrayList<String> stringArrayList = new ArrayList<>();
+
+        public void addFragment(Fragment fragment , String string){
+            fragmentArrayList.add(fragment);
+            stringArrayList.add(string);
+        }
+
+
+        public MainAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentArrayList.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentArrayList.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return stringArrayList.get(position);
+        }
+    }
+}
