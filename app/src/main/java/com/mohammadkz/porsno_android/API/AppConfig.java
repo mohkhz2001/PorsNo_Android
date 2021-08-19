de6d960e01dbc3fd041389267e47efa37bf9a23e
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-08-19 23:07:46 +0430

    handle string added

--- app/src/main/java/com/mohammadkz/porsno_android/API/AppConfig.java
+++ app/src/main/java/com/mohammadkz/porsno_android/API/AppConfig.java
@@ -2,6 +2,7 @@ package com.mohammadkz.porsno_android.API;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class AppConfig {
     private static Retrofit retrofit;
@@ -10,6 +11,7 @@ public class AppConfig {
     public static Retrofit getRetrofit() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder().baseUrl(url)
+                    .addConverterFactory(ScalarsConverterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create()).build();
         }
 
