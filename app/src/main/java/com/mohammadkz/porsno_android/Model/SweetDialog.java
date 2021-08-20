d3cd8a7c93458409ff8f074dc089a7099e7ad09b
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-08-20 11:30:08 +0430

    sweet dialog

---
+++ app/src/main/java/com/mohammadkz/porsno_android/Model/SweetDialog.java
@@ -0,0 +1,37 @@
+package com.mohammadkz.porsno_android.Model;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class SweetDialog {
+
+    static SweetAlertDialog sweetAlertDialog;
+
+    public static void setSweetDialog(SweetAlertDialog sweetDialog, String title, String text) {
+        sweetAlertDialog = sweetDialog;
+        sweetAlertDialog.setTitle(title);
+        sweetAlertDialog.setContentText(text);
+    }
+
+    public static void setSweetDialog(SweetAlertDialog sweetDialog) {
+        sweetAlertDialog = sweetDialog;
+    }
+
+    public static SweetAlertDialog getSweetAlertDialog() {
+        return sweetAlertDialog;
+    }
+
+    public static void startProgress() {
+        sweetAlertDialog.changeAlertType(SweetAlertDialog.PROGRESS_TYPE);
+        sweetAlertDialog.show();
+    }
+
+    public static void stopProgress() {
+        sweetAlertDialog.dismiss();
+    }
+
+    public static void changeSweet(int type, String title, String text) {
+        sweetAlertDialog.changeAlertType(type);
+        sweetAlertDialog.setTitle(title);
+        sweetAlertDialog.setContentText(text);
+    }
+}
