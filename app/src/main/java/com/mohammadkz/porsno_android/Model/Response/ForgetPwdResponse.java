9b63c43d2fc7cf1172011307648046fcc8bb4aa1
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-08-22 01:53:38 +0430

    forget pwd response

---
+++ app/src/main/java/com/mohammadkz/porsno_android/Model/Response/ForgetPwdResponse.java
@@ -0,0 +1,29 @@
+package com.mohammadkz.porsno_android.Model.Response;
+
+public class ForgetPwdResponse {
+    private String status_code , message , code;
+
+    public String getStatus_code() {
+        return status_code;
+    }
+
+    public void setStatus_code(String status_code) {
+        this.status_code = status_code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
