fdf6faa33e25c63db9bef542095869e9e40a5993
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-08-31 20:29:36 +0430

    message added

--- app/src/main/java/com/mohammadkz/porsno_android/Model/Response/NewQuestionaire.java
+++ app/src/main/java/com/mohammadkz/porsno_android/Model/Response/NewQuestionaire.java
@@ -1,7 +1,7 @@
 package com.mohammadkz.porsno_android.Model.Response;
 
 public class NewQuestionaire {
-    String status_code;
+    String status_code , message;
 
 
     public String getStatus_code() {
@@ -10,5 +10,13 @@ public class NewQuestionaire {
 
     public void setStatus_code(String status_code) {
         this.status_code = status_code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
