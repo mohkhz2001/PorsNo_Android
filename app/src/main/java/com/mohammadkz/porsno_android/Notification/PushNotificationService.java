c509ba998e34b4da9b0ae6e093b20b26c3088f58
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-09-03 20:24:54 +0430

    handle the data that came from the server

--- app/src/main/java/com/mohammadkz/porsno_android/Notification/PushNotificationService.java
+++ app/src/main/java/com/mohammadkz/porsno_android/Notification/PushNotificationService.java
@@ -2,7 +2,9 @@ package com.mohammadkz.porsno_android.Notification;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
@@ -14,20 +16,43 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.mohammadkz.porsno_android.API.ApiConfig;
+import com.mohammadkz.porsno_android.API.AppConfig;
+import com.mohammadkz.porsno_android.Activity.AnswerActivity;
 import com.mohammadkz.porsno_android.Activity.LoginActivity;
+import com.mohammadkz.porsno_android.Activity.WebLinkActivity;
+import com.mohammadkz.porsno_android.Model.Response.LoginResponse;
+import com.mohammadkz.porsno_android.Model.SweetDialog;
+import com.mohammadkz.porsno_android.Model.User;
 import com.mohammadkz.porsno_android.R;
+import com.mohammadkz.porsno_android.StaticFun;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PushNotificationService extends FirebaseMessagingService {
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        topic();
+
         String title = remoteMessage.getNotification().getTitle();
         String text = remoteMessage.getNotification().getBody();
-
+        Map<String, String> data = remoteMessage.getData();
         // Create an explicit intent for an Activity in your app
-        Intent intent = new Intent(this, LoginActivity.class); //  activity will be open when tap on the notif
+        Intent intent = new Intent(this, NotificationHandlerActivity.class); //  activity will be open when tap on the notif
+        if (!data.isEmpty()) {
+            intent.putExtra("qId", data.get("qId"));
+            Log.e("qId", data.get("qId"));
+        }
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
@@ -45,32 +70,6 @@ public class PushNotificationService extends FirebaseMessagingService {
 // notificationId is a unique int for each notification that you must define
         notificationManager.notify(1, builder.build());
 
-
-    }
-
-    private void notification(String title, String text) {
-        // Create an explicit intent for an Activity in your app
-        Intent intent = new Intent(this, LoginActivity.class); //  activity will be open when tap on the notif
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "101")
-                .setSmallIcon(R.drawable.icon)
-                .setContentTitle(title)
-                .setContentText(text)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                // Set the intent that will fire when the user taps the notification
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-// notificationId is a unique int for each notification that you must define
-        notificationManager.notify(1, builder.build());
-    }
-
-    // create the topic in app
-    private void topic() {
         FirebaseMessaging.getInstance().subscribeToTopic("offer")
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
@@ -78,5 +77,13 @@ public class PushNotificationService extends FirebaseMessagingService {
 
                     }
                 });
+        FirebaseMessaging.getInstance().subscribeToTopic("new-question")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
     }
+
 }
