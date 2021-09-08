5dfc44e598b0368964e2671d0f8b4f566e94dec7
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-09-08 10:43:48 +0430

    the file name changed and text added

---
+++ app/src/main/java/com/mohammadkz/porsno_android/Fragment/AboutUsFragment.java
@@ -0,0 +1,65 @@
+package com.mohammadkz.porsno_android.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mohammadkz.porsno_android.R;
+
+
+public class ContactUsFragment extends Fragment {
+
+    View view;
+    TextView email, github;
+
+    public ContactUsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_contact_us, container, false);
+
+        initViews();
+        controllerViews();
+
+        return view;
+    }
+
+    private void initViews() {
+        email = view.findViewById(R.id.email);
+        github = view.findViewById(R.id.github);
+
+    }
+
+    private void controllerViews() {
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(android.content.Intent.EXTRA_EMAIL, "eng.khajehzadeh@gmail.com");
+                startActivity(intent);
+            }
+        });
+
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                startActivity(intent);
+            }
+        });
+    }
+}
