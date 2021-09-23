aa98d6d55f5d676beab6940146dcdd198e45a584
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-09-23 23:16:28 +0330

    this file in renamed from my question fragment to home in this fragment will be show the user question , some info of the user info and user can upgrade account

--- app/src/main/java/com/mohammadkz/porsno_android/Fragment/HomeFragment.java
+++ app/src/main/java/com/mohammadkz/porsno_android/Fragment/HomeFragment.java
@@ -2,67 +2,62 @@ package com.mohammadkz.porsno_android.Fragment;
 
 import static android.content.Context.MODE_PRIVATE;
 
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.material.bottomsheet.BottomSheetDialog;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.mohammadkz.porsno_android.API.ApiConfig;
 import com.mohammadkz.porsno_android.API.AppConfig;
 import com.mohammadkz.porsno_android.Activity.AnswerActivity;
 import com.mohammadkz.porsno_android.Activity.EditQuestionActivity;
-import com.mohammadkz.porsno_android.Activity.MainPageActivity;
 import com.mohammadkz.porsno_android.Activity.NewQuestionActivity;
 import com.mohammadkz.porsno_android.Activity.QuestionnaireManagerActivity;
 import com.mohammadkz.porsno_android.Adapter.QuestionAdapter;
-import com.mohammadkz.porsno_android.Model.Question;
-import com.mohammadkz.porsno_android.Model.Questionnaire;
 import com.mohammadkz.porsno_android.Model.Response.GetQuestionResponse;
 import com.mohammadkz.porsno_android.Model.SweetDialog;
 import com.mohammadkz.porsno_android.Model.User;
 import com.mohammadkz.porsno_android.R;
 import com.mohammadkz.porsno_android.StaticFun;
 
+import org.joda.time.Duration;
+import org.joda.time.Interval;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 import es.dmoral.toasty.Toasty;
-import ir.hamsaa.persiandatepicker.PersianDatePickerDialog;
-import ir.hamsaa.persiandatepicker.api.PersianPickerDate;
-import ir.hamsaa.persiandatepicker.api.PersianPickerListener;
-import ir.hamsaa.persiandatepicker.util.PersianCalendarUtils;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MyQuestionFragment extends Fragment {
+public class HomeFragment extends Fragment {
 
     View view;
     RecyclerView myQuestionList;
     User user;
-    TextView emptyList;
+    TextView emptyList, userName, questionLeft, daysLeft;
+    Button reBuy;
+    CardView newQuestion;
     ApiConfig request;
     SwipeRefreshLayout swipeRefresh;
     BottomSheetDialog bottomSheetDialogMore;
@@ -70,7 +65,7 @@ public class MyQuestionFragment extends Fragment {
     GetQuestionResponse questionResponse;
 
 
-    public MyQuestionFragment(User user) {
+    public HomeFragment(User user) {
         // Required empty public constructor
         this.user = user;
     }
@@ -80,7 +75,7 @@ public class MyQuestionFragment extends Fragment {
                              Bundle savedInstanceState) {
         try {
             // Inflate the layout for this fragment
-            view = inflater.inflate(R.layout.fragment_my_question, container, false);
+            view = inflater.inflate(R.layout.fragment_home, container, false);
 
             request = AppConfig.getRetrofit().create(ApiConfig.class);
 
@@ -89,6 +84,7 @@ public class MyQuestionFragment extends Fragment {
             initViews();
             controllerView();
             getData();
+            setValue();
             bottomSheetMore();
 
             return view;
@@ -105,6 +101,11 @@ public class MyQuestionFragment extends Fragment {
         myQuestionList.setHasFixedSize(true);
         emptyList = view.findViewById(R.id.emptyList);
         swipeRefresh = view.findViewById(R.id.swipeRefresh);
+        userName = view.findViewById(R.id.userName);
+        questionLeft = view.findViewById(R.id.questionLeft);
+        daysLeft = view.findViewById(R.id.daysLeft);
+        reBuy = view.findViewById(R.id.reBuy);
+        newQuestion = view.findViewById(R.id.newQuestion);
     }
 
     private void controllerView() {
@@ -113,6 +114,23 @@ public class MyQuestionFragment extends Fragment {
             public void onRefresh() {
                 getData();
                 swipeRefresh.setRefreshing(false);
+            }
+        });
+
+        reBuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        newQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Integer.parseInt(user.getQuestionRemaining()) > 0)
+                    newQuestion();
+                else
+                    Toasty.error(getContext(), "پرسشنامه های شما به اتمام رسیده است.", Toasty.LENGTH_LONG, true).show();
             }
         });
     }
@@ -335,5 +353,42 @@ public class MyQuestionFragment extends Fragment {
                 "\n * این پیام صرفا جهت تست توسط برنامه ارسال می شود *\n";
         intent.putExtra(intent.EXTRA_TEXT, body);
         startActivity(Intent.createChooser(intent, "choose app"));
+    }
+
+    private void setValue() {
+        userName.setText(user.getName());
+        questionLeft.setText(user.getQuestionRemaining());
+        daysLeft.setText(parseDate());
+    }
+
+    // calculate the left day of the user account
+    private String parseDate() {
+        try {
+            Timestamp today = new Timestamp(System.currentTimeMillis());
+
+            SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+            Date end = new Date(Long.parseLong(user.getEndTime()));
+            System.out.println(sf.format(end));
+
+            Interval interval = new Interval(today.getTime(), Long.parseLong(user.getEndTime() + "000"));
+            Duration period = interval.toDuration();
+
+            return period.getStandardDays() + "";
+        } catch (Exception e) {
+            StaticFun.setLog((user == null) ? "-"
+                            : (user.getPn().length() > 0 ? user.getPn() : "-"),
+                    e.getMessage().toString()
+                    , "re buy fragment - parse date ");
+
+            return "---";
+        }
+    }
+
+    private void newQuestion() {
+        Intent intent = new Intent(getContext(), NewQuestionActivity.class);
+        Gson gson = new Gson();
+        String a = gson.toJson(user);
+        intent.putExtra("userInfo", a);//user.getID()
+        startActivity(intent);
     }
 }
